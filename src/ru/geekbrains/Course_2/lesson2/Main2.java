5729b4cfa1a13e74eac984ebac884640a210a160
Kotuma Alexander <aikotuma@gmail.com>
2019-06-19 22:58:48 +0400

    Lesson2 started

--- src/ru/geekbrains/Course_2/lesson2/Main2.java
+++ src/ru/geekbrains/Course_2/lesson2/Main2.java
@@ -2,6 +2,6 @@ package ru.geekbrains.Course_2.lesson2;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("args = [" + 2 + "]");
+        System.out.println("args = [" + 2 + "]"); //
     }
 }
